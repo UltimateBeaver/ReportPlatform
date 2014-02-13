90ee6f309a6b4fc8b4e84f3dcd5bc7b74d575fce
UltimateBeaver <tlhly@hotmail.com>
2014-02-13 20:26:42 +0800

    add merges

--- platforms/android/bin/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/a/Downloads/ReportPlatform/platforms/android/bin/HelloWorld-debug-unaligned.apk : \
-/Users/a/Downloads/ReportPlatform/platforms/android/bin/HelloWorld.ap_ \
-/Users/a/Downloads/ReportPlatform/platforms/android/bin/classes.dex \
-/Users/a/Downloads/ReportPlatform/platforms/android/CordovaLib/bin/classes.jar \
